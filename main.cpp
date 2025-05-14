9f2a5b98b266f53aeb8ae1a588416c53b70dd06c
Pshenich Andrian <andriod3107@mail.ru>
2025-05-14 20:05:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <Windows.h>
+#include <set>
+#include <map>
+#include <cmath>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);                               
+	SetConsoleOutputCP(1251);
+
+
+	return 0;
+}
