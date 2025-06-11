8cd59bd27a8777f2bfc4e738f433ac571955fae4
Pshenich Andrian <andriod3107@mail.ru>
2025-06-11 22:47:45 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,16 +1,13 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-// Размер игрового поля
-const int FIELD_SIZE = 10;
 
-// Символы для отображения состояния клеток
+const int FIELD_SIZE = 10; // Размер игрового поля
 const char EMPTY = ' ';    // Пустая клетка
 const char SHIP = 's';     // Корабль
 const char HIT = 'x';      // Попадание
 const char MISS = 'o';     // Промах
 
-// Уровень сложности компьютера
 enum class Difficulty {
     Random,  // Случайные ходы
     Smart    // "Умные" ходы с запоминанием попаданий
