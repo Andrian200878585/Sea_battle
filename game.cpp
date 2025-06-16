2f19831cca06e6c07e54f977e237a2586fb37ceb
Pshenich Andrian <andriod3107@mail.ru>
2025-06-16 14:26:05 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -6,12 +6,17 @@
 #include <cstdlib>
 #include <ctime>
 
+// Убираем конфликтующий макрос max из Windows.h
+#undef max
+
 using namespace std;
 
 // Конструктор игры
-Game::Game(GameMode m, Difficulty d) : mode(m), difficulty(d), player1Turn(true) {}
+Game::Game(GameMode m, Difficulty d) : mode(m), difficulty(d), player1Turn(true) {} //mode(m)	m (аргумент)	Устанавливает режим игры (PvP или PvC).
+//difficulty(d)	d(аргумент)	Устанавливает сложность компьютера(Random или Smart).
+//player1Turn(true)	true	Указывает, что первый ход принадлежит игроку 1.
 
-// Настройка игровых полей
+
 void Game::setupFields() {
     cout << "\nНастройка поля для Игрока 1:\n";
     setupPlayerField(player1Field);
@@ -20,25 +25,26 @@ void Game::setupFields() {
         clearScreen();
         cout << "Настройка поля для Игрока 2:\n";
         setupPlayerField(player2Field);
-    } else {
+    }
+    else {
         setupComputerField();
     }
 }
 
-// Настройка поля игрока
 void Game::setupPlayerField(Field& field) {
     cout << "Как вы хотите расставить корабли?\n(1 - автоматически, 2 - вручную): ";
     int choice;
     cin >> choice;
 
-    vector<int> shipSizes = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+    vector<int> shipSizes = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
 
     if (choice == 1) {
         // Автоматическая расстановка
         for (int size : shipSizes) {
             while (!field.placeShipAuto(size)) {}
         }
-    } else {
+    }
+    else {
         // Ручная расстановка
         for (int size : shipSizes) {
             bool placed = false;
@@ -52,7 +58,7 @@ void Game::setupPlayerField(Field& field) {
 
 // Настройка поля компьютера
 void Game::setupComputerField() {
-    vector<int> shipSizes = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+    vector<int> shipSizes = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
     for (int size : shipSizes) {
         while (!player2Field.placeShipAuto(size)) {}
     }
@@ -68,14 +74,16 @@ void Game::play() {
                 cout << "Игрок 1 победил!\n";
                 break;
             }
-        } else {
+        }
+        else {
             if (mode == GameMode::PvP) {
                 playerTurn("Игрок 2", player2Field, player1Field);
                 if (player1Field.allShipsDestroyed()) {
                     cout << "Игрок 2 победил!\n";
                     break;
                 }
-            } else {
+            }
+            else {
                 computerTurn();
                 if (player1Field.allShipsDestroyed()) {
                     cout << "Компьютер победил!\n";
@@ -101,8 +109,8 @@ void Game::playerTurn(const string& playerName, Field& playerField, Field& oppon
         cout << "Введите координаты выстрела (вертик. ; горизонт.): ";
         cin >> x >> y;
     } while (!opponentField.isValidPosition(x, y) ||
-             opponentField.field[x][y] == HIT ||
-             opponentField.field[x][y] == MISS);
+        opponentField.field[x][y] == HIT ||
+        opponentField.field[x][y] == MISS);
 
     bool hit = opponentField.shootAt(x, y);
     cout << (hit ? "Попадание!" : "Промах!") << endl;
@@ -119,20 +127,21 @@ void Game::computerTurn() {
             x = rand() % FIELD_SIZE;
             y = rand() % FIELD_SIZE;
         } while (player1Field.field[x][y] == HIT || player1Field.field[x][y] == MISS);
-    } else {
+    }
+    else {
         // Умная стратегия
         pair<int, int> lastHit = computerHits.back();
         vector<pair<int, int>> possibleTargets;
 
         // Проверка соседних клеток
-        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int dirs[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
         for (auto& dir : dirs) {
             int nx = lastHit.first + dir[0];
             int ny = lastHit.second + dir[1];
             if (player1Field.isValidPosition(nx, ny) &&
                 player1Field.field[nx][ny] != HIT &&
                 player1Field.field[nx][ny] != MISS) {
-                possibleTargets.push_back({nx, ny});
+                possibleTargets.push_back({ nx, ny });
             }
         }
 
@@ -140,7 +149,8 @@ void Game::computerTurn() {
             auto target = possibleTargets[rand() % possibleTargets.size()];
             x = target.first;
             y = target.second;
-        } else {
+        }
+        else {
             computerHits.clear();
             computerTurn();
             return;
@@ -152,7 +162,7 @@ void Game::computerTurn() {
     cout << (hit ? "Попадание!" : "Промах!") << endl;
 
     if (hit) {
-        computerHits.push_back({x, y});
+        computerHits.push_back({ x, y });
     }
 }
 
